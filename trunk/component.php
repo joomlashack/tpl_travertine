116bf71a5083e73b0ba7ad99d4fff06962bd226d
TJ Baker <tj@joomlashack.com>
2011-10-17 18:34:40 +0000

    Fixed borked component.php file

--- trunk/component.php
+++ trunk/component.php
@@ -6,15 +6,19 @@
  * 
  * Single Component view file
  */
+
 // no direct access
 defined( '_JEXEC' ) or die( 'Restricted access' );
+
 ?>
-<doctype>
-<html>
-<head>
-<w:head />
-</head>
-<body>
-	<w:content />
-</body>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
+   <head>
+       <jdoc:include type="head" />
+   </head>
+   <body class="contentpane">
+       <jdoc:include type="message" />
+       <jdoc:include type="component" />
+   </body>
 </html>
+
