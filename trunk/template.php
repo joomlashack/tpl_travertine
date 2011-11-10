0c858cfb1897ab30a53ecf428af0597d815d4103
TJ Baker <tj@joomlashack.com>
2011-11-10 18:35:46 +0000

    Switched from jQuery to Mootools for full size background

--- trunk/template.php
+++ trunk/template.php
@@ -10,13 +10,13 @@
  */
 // no direct access
 defined('_JEXEC') or die('Restricted access');
+JHTML::_ ( 'behavior.mootools' );
 ?>
 <doctype>
 <html>
 <head>
 <w:head />
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js"></script>
-<script type="text/javascript" src="<?php echo $this->baseurl ?>/templates/js_travertine/js/jquery.fullbg.min.js"></script>
+<script type="text/javascript" src="<?php echo $this->baseurl ?>/templates/js_travertine/js/mootools.fullbg.min.js"></script>
 </head>
 <body>
 <img src="<?php echo $this->baseurl ?>/templates/js_travertine/images/<?php echo $this->params->get('style') ?>/bg.jpg" id="background" alt="">
@@ -108,10 +108,6 @@ defined('_JEXEC') or die('Restricted access');
     </div>
   </div>
 </div>
-<script type="text/javascript">
-$(window).load(function() {
-	$("#background").fullBg();
-});
 </script>
 <?php //echo $this->document->params->get("footerscript",""); ?>
 </body>
