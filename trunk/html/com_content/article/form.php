46f820806ec0e0b41a18efbfb43dd9cc80bc028d
Vishal Grover <vishal@joomlashack.com>
2012-04-30 06:59:45 +0000

    travertine Discretionary Replaced content form.php override file for Joomla 2.5 compatibility when layout changed to "form"

--- trunk/html/com_content/article/form.php
+++ trunk/html/com_content/article/form.php
@@ -5,7 +5,15 @@
  * the default set of overrides we have in our template framework. 
  */
 
-$app =& JFactory::getApplication();
+$app = JFactory::getApplication();
 
 require_once(JPATH_THEMES.DS.$app->getTemplate().DS.'wright'.DS.'html'.DS.'overrider.php');
-include(Overrider::getOverride('com_content.article', 'form'));
+
+// Specific override.  The support for form.php was dropped since 1.6, so it's redirected to default.php.  Augusto Alvarez.  Apr 2012
+$version = Overrider::getVersion();
+if ($version[0] <= "1" && $version[1] <= "5") {
+	include(Overrider::getOverride('com_content.article', 'form'));
+}
+else {
+	include(JPATH_THEMES.DS.$app->getTemplate().DS.'html'.DS.'com_content'.DS.'article'.DS.'default.php');
+}
